f558aeedf657b7a5d5229976aa06511e43ccc7bd
ruanyudi <977953751@qq.com>
2023-06-23 11:06:34 +0000

    Update

--- 03_VGG/model.py
+++ 03_VGG/model.py
@@ -32,7 +32,7 @@ class VGG(nn.Module):
             nn.ReLU(), nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(), nn.Dropout(),
-            nn.Linear(4096, 10)
+            nn.Linear(4096, 5)
         )
         self.__init__weight()
         print("Model Loaded Done !")
