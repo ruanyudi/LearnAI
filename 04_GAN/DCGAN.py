efce52d7d4e4af2580a006f43a97f1975cc1ae60
ruanyudi <977953751@qq.com>
2023-07-10 11:37:11 +0800

    reformat & fix Bug

--- 04_GAN/DCGAN.py
+++ 04_GAN/DCGAN.py
@@ -7,7 +7,6 @@ DCGAN Tutorial
 
 """
 
-
 ######################################################################
 # Introduction
 # ------------
@@ -112,7 +111,7 @@ DCGAN Tutorial
 # will be explained in the coming sections.
 # 
 
-#%matplotlib inline
+# %matplotlib inline
 import argparse
 import os
 import random
@@ -131,12 +130,11 @@ from IPython.display import HTML
 
 # Set random seed for reproducibility
 manualSeed = 42
-#manualSeed = random.randint(1, 10000) # use if you want new results
+# manualSeed = random.randint(1, 10000) # use if you want new results
 print("Random Seed: ", manualSeed)
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
-torch.use_deterministic_algorithms(True) # Needed for reproducible results
-
+torch.use_deterministic_algorithms(True)  # Needed for reproducible results
 
 ######################################################################
 # Inputs
@@ -211,7 +209,6 @@ beta1 = 0.5
 # Number of GPUs available. Use 0 for CPU mode.
 ngpu = 1
 
-
 ######################################################################
 # Data
 # ----
@@ -246,12 +243,12 @@ ngpu = 1
 # We can use an image folder dataset the way we have it setup.
 # Create the dataset
 dataset = dset.CIFAR100(root='../data_set',
-                           transform=transforms.Compose([
-                               transforms.Resize(image_size),
-                               transforms.CenterCrop(image_size),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                           ]),download=True)
+                        transform=transforms.Compose([
+                            transforms.Resize(image_size),
+                            transforms.CenterCrop(image_size),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                        ]), download=True)
 # Create the dataloader
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                          shuffle=True, num_workers=workers)
@@ -261,11 +258,10 @@ device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else 
 
 # Plot some training images
 real_batch = next(iter(dataloader))
-plt.figure(figsize=(8,8))
+plt.figure(figsize=(8, 8))
 plt.axis("off")
 plt.title("Training Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1, 2, 0)))
 
 
 ######################################################################
@@ -334,7 +330,7 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. ``(ngf*8) x 4 x 4``
@@ -342,15 +338,15 @@ class Generator(nn.Module):
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. ``(ngf*4) x 8 x 8``
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. ``(ngf*2) x 16 x 16``
-            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # state size. ``(ngf) x 32 x 32``
-            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. ``(nc) x 64 x 64``
         )
@@ -442,14 +438,13 @@ netD = Discriminator(ngpu).to(device)
 # Handle multi-GPU if desired
 if (device.type == 'cuda') and (ngpu > 1):
     netD = nn.DataParallel(netD, list(range(ngpu)))
-    
+
 # Apply the ``weights_init`` function to randomly initialize all weights
 # like this: ``to mean=0, stdev=0.2``.
 netD.apply(weights_init)
 
 # Print the model
 print(netD)
-
 
 ######################################################################
 # Loss Functions and Optimizers
@@ -498,7 +493,6 @@ fake_label = 0.
 # Setup Adam optimizers for both G and D
 optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
-
 
 ######################################################################
 # Training
@@ -583,7 +577,7 @@ print("Starting Training Loop...")
 for epoch in range(num_epochs):
     # For each batch in the dataloader
     for i, data in enumerate(dataloader, 0):
-        
+
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         ###########################
@@ -633,25 +627,24 @@ for epoch in range(num_epochs):
         D_G_z2 = output.mean().item()
         # Update G
         optimizerG.step()
-        
+
         # Output training stats
         if i % 50 == 0:
             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
                   % (epoch, num_epochs, i, len(dataloader),
                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-        
+
         # Save Losses for plotting later
         G_losses.append(errG.item())
         D_losses.append(errD.item())
-        
+
         # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+        if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
             with torch.no_grad():
                 fake = netG(fixed_noise).detach().cpu()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
-            
-        iters += 1
 
+        iters += 1
 
 ######################################################################
 # Results
@@ -668,15 +661,14 @@ for epoch in range(num_epochs):
 # Below is a plot of D & G’s losses versus training iterations.
 # 
 
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(10, 5))
 plt.title("Generator and Discriminator Loss During Training")
-plt.plot(G_losses,label="G")
-plt.plot(D_losses,label="D")
+plt.plot(G_losses, label="G")
+plt.plot(D_losses, label="D")
 plt.xlabel("iterations")
 plt.ylabel("Loss")
 plt.legend()
 plt.show()
-
 
 ######################################################################
 # **Visualization of G’s progression**
@@ -687,14 +679,13 @@ plt.show()
 # animation.
 # 
 
-#%%capture
-fig = plt.figure(figsize=(8,8))
+# %%capture
+fig = plt.figure(figsize=(8, 8))
 plt.axis("off")
-ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+ims = [[plt.imshow(np.transpose(i, (1, 2, 0)), animated=True)] for i in img_list]
 ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
 
 HTML(ani.to_jshtml())
-
 
 ######################################################################
 # **Real Images vs. Fake Images**
@@ -707,19 +698,18 @@ HTML(ani.to_jshtml())
 real_batch = next(iter(dataloader))
 
 # Plot the real images
-plt.figure(figsize=(15,15))
-plt.subplot(1,2,1)
+plt.figure(figsize=(15, 15))
+plt.subplot(1, 2, 1)
 plt.axis("off")
 plt.title("Real Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(), (1, 2, 0)))
 
 # Plot the fake images from the last epoch
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 plt.axis("off")
 plt.title("Fake Images")
-plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+plt.imshow(np.transpose(img_list[-1], (1, 2, 0)))
 plt.show()
-
 
 ######################################################################
 # Where to Go Next
@@ -735,4 +725,4 @@ plt.show()
 #    `here <https://github.com/nashory/gans-awesome-applications>`__
 # -  Create GANs that generate
 #    `music <https://www.deepmind.com/blog/wavenet-a-generative-model-for-raw-audio/>`__
-# 
+#
--- 04_GAN/GAN.py
+++ 04_GAN/GAN.py
@@ -8,12 +8,13 @@ import matplotlib.pyplot as plt
 # from model.FeatureExtractor import FeatureExtractor
 from model.Discriminator import Discriminator
 from model.Generator import Generator
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # class FeatureExtractor(FeatureExtractor):
 #     def forward(self,x):
 #         return self.features(x)
-        
+
 
 data_transforms = {
     'train': transforms.Compose([
@@ -25,7 +26,7 @@ data_transforms = {
     'flower': transforms.Compose([
         transforms.RandomResizedCrop(64),
         transforms.ToTensor(),
-        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 }
 
@@ -34,13 +35,14 @@ def show_img(model):
     fig = plt.figure(figsize=(10, 10))
     noise_input = torch.randn((100, 100), device=device)
     prediction = model(noise_input).detach().cpu()
-    prediction = torch.transpose(prediction,1,3).numpy()
-    
+    prediction = prediction.permute(1, 2, 0).numpy()
+
     for i in range(100):
         plt.subplot(10, 10, i + 1)
         plt.imshow((prediction[i] + 1) / 2, cmap='gray')
         plt.axis('off')
     plt.show()
+
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -51,16 +53,18 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 
-def loss_optical_fn(x,y):
+def loss_optical_fn(x, y):
     # batch_size*256*4*4
-    x,y = x.flatten(0),y.flatten(0)
-    return (torch.abs(x-y)).mean()
+    x, y = x.flatten(0), y.flatten(0)
+    return (torch.abs(x - y)).mean()
 
-def train(epochs,lr):
+
+def train(epochs, lr):
     # trainData = torchvision.datasets.CIFAR10('../data_set', download=True, transform=data_transforms['train'], train=True)
-    trainData = torchvision.datasets.MNIST('../data_set', download=True, transform=data_transforms['train'], train=False)
+    trainData = torchvision.datasets.MNIST('../data_set', download=True, transform=data_transforms['train'],
+                                           train=False)
     # trainData = torchvision.datasets.ImageFolder('../data_set/flower_data/flower_photos',transform = data_transforms['flower'])
-    trainDataLoader = DataLoader(trainData, batch_size=128, shuffle=False,num_workers=16)
+    trainDataLoader = DataLoader(trainData, batch_size=128, shuffle=False, num_workers=16)
     Gen = Generator(out_channels=1).to(device)
     Gen.apply(weights_init)
     Dis = Discriminator(in_channels=1).to(device)
@@ -74,8 +78,8 @@ def train(epochs,lr):
     # except:
     #     print("Fail to Loader the Weight")
     # print(featureExtractor(dummy_input).shape)
-    G_optimizer = torch.optim.Adam(Gen.parameters(), lr=lr,betas=(0.5,0.999))
-    D_optimizer = torch.optim.Adam(Dis.parameters(), lr=lr,betas=(0.5,0.999))
+    G_optimizer = torch.optim.Adam(Gen.parameters(), lr=lr, betas=(0.5, 0.999))
+    D_optimizer = torch.optim.Adam(Dis.parameters(), lr=lr, betas=(0.5, 0.999))
     loss_fn = torch.nn.BCELoss()
     for epoch in range(epochs):
         G_loss = 0.
@@ -95,21 +99,21 @@ def train(epochs,lr):
             loss_gt = loss_fn(pred_gt, torch.ones_like(pred_gt, device=device))
             loss_ge = loss_fn(pred_ge, torch.zeros_like(pred_ge, device=device))
             loss_D = loss_gt + loss_ge
-            D_loss_GE +=loss_ge
-            D_loss_GT +=loss_gt
+            D_loss_GE += loss_ge
+            D_loss_GT += loss_gt
             D_loss += loss_D
             D_optimizer.zero_grad()
             loss_D.backward()
             D_optimizer.step()
 
-            loss_G = loss_fn(Dis(gen_result), torch.ones((len(features),1), device=device))
+            loss_G = loss_fn(Dis(gen_result), torch.ones((len(features), 1), device=device))
             G_loss += loss_G
             G_optimizer.zero_grad()
             loss_G.backward()
             G_optimizer.step()
-        print(f"Epoch :{epoch} | D_Loss :{D_loss / count} | G_Loss : {G_loss / count} | GT :{D_loss_GT/count} | GE :{D_loss_GE/count}")
-        if(epoch%5==0):
+        print(
+            f"Epoch :{epoch} | D_Loss :{D_loss / count} | G_Loss : {G_loss / count} | GT :{D_loss_GT / count} | GE :{D_loss_GE / count}")
+        if (epoch % 5 == 0):
             show_img(Gen)
         torch.save(Gen, 'Gen.pth')
         torch.save(Dis, 'Dis.pth')
-
